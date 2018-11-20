e2cc85f35220660dce88b0053c8ffafa438beab7
Jorge Vale <ajorgeav11@gmail.com>
2018-11-20 00:42:55 +0000

    added ordertrans

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -1,18 +1,15 @@
 const
 	Op = Sequelize.Op,
 	request = require('request'),
-	rsa = require('node-rsa'),
-	bcrypt = require('bcrypt'),
-	uuidv4 = require('uuid/v4'),
 	User = require('../models/index').User,
 	Ticket = require('../models/index').Ticket,
 	Voucher = require('../models/index').Voucher,
-	Transaction = require('../models/index').Transaction,
+	OrderTransaction = require('../models/index').OrderTransaction,
 	TicketTransaction = require('../models/index').TicketTransaction;
 
 module.exports = {
 	list(req, res) {
-	    var id = req.params.id;
+		var id = req.params.id;
 		console.log(id);
 		User
 			.findOne({
@@ -24,10 +21,6 @@ module.exports = {
 				if (!user) {
 					return res.status(400).json({success: false, message: 'User id doesn\'t exist'});
 				}
-				var ticketTransactions = [];
-				var orderTransactions = [];
-				var validTickets = [];
-				var validVouchers = [];
 				TicketTransaction
 					.findAll({
 						where: {
@@ -35,29 +28,41 @@ module.exports = {
 						}
 					})
 					.then(transactions => {
-						let whereClauseVoucher = {[Op.and]: [{userId: user.id}, {available: true}]};
-						Voucher
+						OrderTransaction
 							.findAll({
-								where: whereClauseVoucher
+								where: {
+									userId : user.id
+								}
 							})
-							.then(vouchers => {
-								let whereClauseTicket = {[Op.and]: [{userId: user.id}, {used: false}]};
-								Ticket
+							.then(orderTransactions=>{
+
+								let whereClauseVoucher = {[Op.and]: [{userId: user.id}, {available: true}]};
+								Voucher
 									.findAll({
-										where: whereClauseTicket
+										where: whereClauseVoucher
 									})
-									.then(tickets => {
-										res.status(200).json({success:true, message:'Valid',
-											ticketTransactions: transactions, orderTransactions: orderTransactions,
-											validTickets: tickets, validVouchers: vouchers});
+									.then(vouchers => {
+										let whereClauseTicket = {[Op.and]: [{userId: user.id}, {used: false}]};
+										Ticket
+											.findAll({
+												where: whereClauseTicket
+											})
+											.then(tickets => {
+												res.status(200).json({success:true, message:'Valid',
+													ticketTransactions: transactions, orderTransactions: orderTransactions,
+													validTickets: tickets, validVouchers: vouchers});
+											})
+											.catch(err => {
+												res.status(400).json({success: false, message: 'Error ocurred: ' + err});
+											});;
 									})
 									.catch(err => {
 										res.status(400).json({success: false, message: 'Error ocurred: ' + err});
-									});;
+									});
 							})
 							.catch(err => {
 								res.status(400).json({success: false, message: 'Error ocurred: ' + err});
-							});;
+							});
 					})
 					.catch(err => {
 						res.status(400).json({success: false, message: 'Error ocurred: ' + err});
